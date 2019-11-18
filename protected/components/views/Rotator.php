fbad6bf276a4d9d4e83ee7bd89cff0488e5c0732
Adam Mazur <adam.mazur@globalapptesting.com>
2019-11-18 23:54:32 +0100

    enable gabriona font face definition and temporarly disable readfile php call

--- protected/components/views/Rotator.php
+++ protected/components/views/Rotator.php
@@ -9,7 +9,7 @@
 <div class="rotator_el">
  <?php echo CHtml::image(Yii::app()->request->baseUrl."/images/photos/4.jpg"); ?>
 	 <h1 id="day_desc" style="background-color:rgba(255,255,255,0.7); color: #333; top:auto; bottom:20px; left:auto; right:-20px; font-size:18px; padding:10px;">
-		<strong style="font-size:24px;">Dziś obchodzimy:</strong><? readfile('http://www.edycja.pl/ext/dzien.php'); ?>
+		<strong style="font-size:24px;">Dziś obchodzimy:</strong><? //readfile('http://www.edycja.pl/ext/dzien.php'); ?>
 		<style>
 			.www_edycja_pl>*{
 				display: none;
